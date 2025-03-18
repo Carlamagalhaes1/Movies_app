66d93ee611d7b7396bbd23673121fc8798b6d2f4
Carla Magalhães <carlamagalhes60@gmail.com>
2025-03-18 11:42:17 -0300

    meu primeiro commit

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,12 @@
+export default function NavBar () {
+    return (
+        <nav className="">
+            <div className="container mx-auto flex items-center bg- ">
+            <h1 className="text-2xl font-bold ">Filmes</h1>
+            </div>
+            
+
+
+        </nav>
+    )
+} 
