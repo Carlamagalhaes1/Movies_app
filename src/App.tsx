0fc241846fb896eff81082e953f66614d9463329
Carla Magalhães <carlamagalhes60@gmail.com>
2025-03-20 20:43:13 -0300

    route, pagina de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import MoviesList from './components/MoviesList';
 import Layout from './layout';
+import Login from './pages/Login/index';
 
 
 function App() {
   return (
     <>
+      <BrowserRouter
+      >
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/movie' element={<Layout><MoviesList/></Layout>}/>
+
+         
+
+        </Routes>
+      </BrowserRouter>
       
       
-      <Layout>
-        <MoviesList />
-      </Layout>
+      
     </>
   );
 }
