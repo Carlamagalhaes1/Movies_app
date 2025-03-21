c52a41dc932f452fcd28cc660dbbda25e680c6ca
Carla Magalhães <carlamagalhes60@gmail.com>
2025-03-20 21:31:44 -0300

    conserto de responsividade

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,9 @@
 export default function Login() {
     return(
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <div className="flex flex-col bg-white shadow-2xl rounded-2xl p-8  space-y-6 w-full max-w-md">
-            <h2 className="text-4xl font-bold mb-3 text-center">Bem vindo</h2>
-            <p className="text-gray-400 mb-8 text-center">Bem-vindo de volta! Por favor, insira seus dados.</p>
+        <div className="flex flex-col bg-white shadow-2xl rounded-2xl p-8  w-full max-w-md">
+            <h2 className="text-4xl font-bold mb-3 ">Bem vindo</h2>
+            <p className="text-gray-400 mb-8 ">Bem-vindo de volta! Por favor, insira seus dados.</p>
             
             <label htmlFor="email" className="text-md mb-2">Email</label>
             <input type="text" id="email" className="w-full p-2 border border-gray-300 rounded-md placeholder-gray-500" />
@@ -21,9 +21,13 @@ export default function Login() {
             
             <button className="w-full bg-black text-white p-3 rounded-lg mt-6 hover:bg-gray-800">Fazer Login</button>
             
-            <button className="w-full border border-gray-300 text-md p-3 rounded-lg mt-4 flex items-center justify-center hover:bg-black hover:text-white">
-                <img src={``} alt="foto" className="w-6 h-6 mr-2" /> 
+            <button className="w-full border border-gray-300 text-md p-3 rounded-lg mt-4 flex items-center justify-center hover:bg-gray-200 hover:text-white">
+                <img src={`https://logopng.com.br/logos/google-37.png`} alt="foto" className="w-6 h-6 mr-2" />Entre com o Google
             </button>
+            <div className="text-center text-gray-400 mt-3">
+                Não tem conta? 
+             <span className="font-bold text-black"> Se inscreva de graça</span>
+            </div>
         </div>
     </div>
        
