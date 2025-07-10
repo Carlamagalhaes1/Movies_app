48bd5253f7a5f7685c2708d2e8bb05cc5256082b
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-07-09 22:33:03 -0300

    Feat: Barra de pesquisa

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,18 @@
-export default function NavBar () {
+export default function NavBar() {
     return (
         <nav className="bg-blue-950 fixed top-0 left-0 right-0 z-50">
-            <div className="container mx-auto flex items-center p-3">
-            <h1 className="text-2xl font-bold text-white  ">Melhores Lançamento de Filmes de 2024/2025</h1>
+            <div className="container mx-auto flex flex-col md:flex-row items-center justify-between p-3 gap-3">
+                <h1 className="text-2xl font-bold text-white">
+                    Melhores Lançamentos de Filmes de 2024/2025
+                </h1>
+                <div className="w-full md:w-1/3">
+                    <input
+                        type="text"
+                        placeholder="Pesquisar filme"
+                        className="w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    />
+                </div>
             </div>
-            
-
-
         </nav>
-    )
-} 
+    );
+}
