e8548a04744f6e376cce2f26e598a5af9d41672c
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-07-17 22:29:15 -0300

    feat: implementa filtro de busca nos filmes

--- src/components/MoviesList/index.tsx
+++ src/components/MoviesList/index.tsx
@@ -2,15 +2,14 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import MovieCard from "../MovieCard";
 import { Movie } from "../../types/movie";
-
-
+import NavBar from "../NavBar";
 
 export default function MoviesList() {
     const [movies, setMovies] = useState<Movie[]>([]);
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
         const getMovies = async () => {
-            
             try {
                 const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
                     params: {
@@ -18,23 +17,27 @@ export default function MoviesList() {
                         language: 'pt-BR',
                     },
                 });
-                
                 setMovies(response.data.results);
             } catch (error) {
                 console.error('Erro ao buscar filmes:', error);
             }
-            
         };
 
         getMovies();
     }, []);
 
+    const filteredMovies = movies.filter((movie) =>
+        movie.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+
     return (
-        <div className="grid grid-cols-3 gap-4  ">
-        {movies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
-        ))}
-    </div>
-    )
-        
+        <div>
+            <NavBar onSearch={setSearchQuery} />
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-20 p-4">
+                {filteredMovies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+        </div>
+    );
 }
